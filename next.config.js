75cfbb8e62099b7a65558d2b7f445feb58054477
timepresent95 <timepresent95@gmail.com>
2024-12-12 18:50:14 +0900

    Fix: 네비게이션 이미지 등록

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["cdn.goorm-ktb-013.goorm.team"], // 허용할 CDN 도메인
+  },
 };
 
 module.exports = nextConfig;
